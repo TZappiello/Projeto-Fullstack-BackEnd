6c3ef3a807f635cbbd3d7b6b5a3fbbb35ffa4f8a
Tzappiello <tzappiello@gmail.com>
2022-06-09 19:52:00 -0300

    Perfil de desenvolvimento criado

---
+++ src/main/java/com/zappiello/helpdesk/config/DevConfig.java
@@ -0,0 +1,29 @@
+package com.zappiello.helpdesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.zappiello.helpdesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String value;
+
+	@Bean
+	public boolean instanciaDB() {
+		if(value.equals("create")) {
+			this.dbService.instanciaDB();
+		}
+		return false;
+	}
+
+}
