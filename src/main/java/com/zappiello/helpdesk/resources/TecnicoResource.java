b5e92299adbc25176560e313c5a3638f2ce77786
Tzappiello <tzappiello@gmail.com>
2022-06-13 20:44:16 -0300

    Padrão DTO para Tecnico implementado.

--- src/main/java/com/zappiello/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/zappiello/helpdesk/resources/TecnicoResource.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.zappiello.helpdesk.domain.Tecnico;
+import com.zappiello.helpdesk.domain.dtos.TecnicoDTO;
 import com.zappiello.helpdesk.services.TecnicoService;
 
 @RestController
 @RequestMapping(value = "/tecnicos")
 public class TecnicoResource {
-
-	//localhost:8080/tecnicos/1
+				
 	
 	@Autowired
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }
