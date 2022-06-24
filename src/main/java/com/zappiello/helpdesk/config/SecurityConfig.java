13e6c83b49e5f70cfa4ee4cb9f2523cccbe6a4d4
lailaz <88406056+zallih@users.noreply.github.com>
2022-06-24 12:06:06 -0300

    Autorização com JWT implementado

--- src/main/java/com/zappiello/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/zappiello/helpdesk/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,9 +18,11 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.zappiello.helpdesk.security.JWTAuthenticationFilter;
+import com.zappiello.helpdesk.security.JWTAuthorizationFilter;
 import com.zappiello.helpdesk.security.JWTUtil;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
@@ -41,8 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		// http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,
-		// userDetailsService));
+		 http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,
+		 userDetailsService));
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
