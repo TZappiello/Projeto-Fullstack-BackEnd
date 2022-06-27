7b166f0a09382dc076232e9775e9b74f5a4ae071
Tzappiello <tzappiello@gmail.com>
2022-06-27 20:23:43 -0300

    Ajuste na validação do token resposta do Postman

--- src/main/java/com/zappiello/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/zappiello/helpdesk/config/SecurityConfig.java
@@ -44,8 +44,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 		http.cors().and().csrf().disable();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		 http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,
-		 userDetailsService));
+		 http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
